4431183439981a2972cb8a19a34c9cad22388d72
pekashy <klever.kl@yandex.ru>
2018-02-23 22:36:02 +0300

    public function headers added

--- structure.h
+++ structure.h
@@ -3,3 +3,30 @@
 int funcThatReturns5 (){
 	return 7;
 }
+
+typedef struct Node{
+	int leaf;
+	int n;
+	int key;
+	Node c[];
+} NODE;
+
+typedef struct Tree{
+	int t;
+	Node root;
+} TREE;
+
+NODE* addElement(){
+
+}
+
+int deleteElement(){
+
+}
+
+NODE* getElement(){
+
+}
+
+//TODO: iterator
+//TODO: getMax
