3efa93d04d006e1ef2af054112e5dc178d154e9a
pekashy <klever.kl@yandex.ru>
2018-02-28 04:12:08 +0300

    Stupid memory bugs fixed. TODO: fix node inserting mech

--- structure.c
+++ structure.c
@@ -9,20 +9,29 @@
 int main(){
 	char filename[]="source";
 	int file=open(filename, O_RDONLY);
-	struct stat st;
+    printf("hello %d\n", file);
+    struct stat st;
 	stat(filename, &st);
-	char* word=malloc(st.st_size*sizeof(char));
-	char* beg=word;
+    printf("hello %d\n", st.st_size);
+    char* word=malloc(((int) st.st_size)*sizeof(char));
+	char* it=word;
 	char c;
+    //printf("hello %d\n", sizeof(TREE));
+    TREE* T=createBtree(3);
+    printf("hello\n");
 	while(read(file, &c, 1)!=0){
-		if(c==' ' || c=='\n' || c=='-'){
-			addNode(word);
-			word=beg;
+		if(c==' ' || c=='\n' || c=='-' && strlen(word)!=0){
+			addNode(T, word);
+            printf("%s\n", word);
+            memset(word, '\0', ((int) st.st_size)*sizeof(char));
+			it=word;
 		}
-		if(c>=65 && c<=90) tolower(c);
-		if(c>=97 && c<=122){
-			*word=c;
-			word++;
+		if(c>=65 && c<=90) c=(char) tolower(c);
+        //printf("%c %d %s\n", c, c, word);
+        if(c>=97 && c<=122){
+			*it=c;
+			it++;
 		}
 	}
+
 }
--- structure.h
+++ structure.h
@@ -1,21 +1,17 @@
 #include <stdio.h>
 #include <string.h>
 
-int funcThatReturns5 (){
-	return 7;
-}
-
 typedef struct Node NODE;
 
 typedef struct Key{
     char* key;
-    int n;
+    int data;
 } KEY;
 
 struct Node{
 	int leaf; //is leaf (0 or 1)
 	int n; //number of node's keys
-	KEY* key; //node's keys, borders, that split subtrees values |2t-1 max|
+	KEY** key; //node's keys, borders, that split subtrees values |2t-1 max|
 	NODE** c; //pointers to the node's children  |2t max|
 };
 
@@ -24,41 +20,46 @@ typedef struct Tree{
 	NODE* root;
 } TREE;
 
+NODE * BTreeInsert(TREE *T, KEY* k);
 
 
 NODE* addNode(TREE* T, char* word){
-
+    KEY* k=malloc(sizeof(KEY));
+    k->key=malloc(sizeof(char)*strlen(word));
+    strcpy(k->key, word);
+    //k->key=word;
+    k->data=1;
+    NODE* s=BTreeInsert(T, k);
+    return s;
 }
 
-
-void BTreeInsert(TREE* T, KEY k){
-	NODE* r=T->root;
-	if(r->n==(2*T->t-1)){ //if the root has maximum number of children
-		//NODE* s=(NODE*) malloc(sizeof(NODE));//ALLOCATING FOR THE NEW ROOT | NO ALLOCATION FOR KEYS; n=0
-		//s->c=(NODE**) malloc(sizeof(NODE*)*(s->n+1));//ALLOCATING FOR THE CHILDREN LINKS
-        NODE* s=allocNode(1);
-		T->root=s;
-		s->leaf=0;
-		s->n=0;
-		s->c[0]=r;//growing up a new root, pointing the old one to it as a child
-		BTreeSplitChild(s, 0, r); //connecting to the main root to the first position; i IS THE INDEX NOT THE NUMBER OF POSITION
-		BTreeInsertNonfull(s, k, T->t);
-	}
-	else
-		BTreeInsertNonfull(r,k,T->t);
-}
-
-NODE* allocNode(int n){
+NODE* allocNode(int n, TREE* T){
 	NODE* node=	(NODE*) malloc(sizeof(NODE));
-	node->c=(NODE**) malloc((n+1)*sizeof(NODE*));
-    node->key=(KEY*) malloc(n*sizeof(KEY));
-    node->key->n=1;
+	node->c=(NODE**) malloc((2*T->t)*sizeof(NODE*));
+    /*for(int i=0; i<n; i++){
+        node->c[i]=(NODE*) malloc(sizeof(NODE));
+    }*/
+    node->key=(KEY**) malloc((2*T->t-1)*sizeof(KEY*));
+    for(int i=0; i<n; i++){
+        node->key[i]=malloc(n*sizeof(KEY));
+        node->key[i]->data=1;
+    }
     return node;
 }
 
-void BTreeSplitChild(NODE* x, int i, NODE* y){//y is full node; x is its nonfull parent x->c[i]=y; z is its new parent
+int compareKeys(KEY* key1, KEY* key2){
+    int i=0;
+    while(*(key1->key+i)==*(key2->key+i)){
+        i++;
+        if(*(key1->key+i)=='\0' || *(key2->key+i)=='\0') return 0;
+    }
+    if(*(key1->key+i)>*(key2->key+i)) return 1;
+    else return -1;
+}
+
+void BTreeSplitChild(NODE* x, int i, NODE* y, TREE* T){//y is full node; x is its nonfull parent x->c[i]=y; z is its new parent
     int t= (y->n+1)/2-1;
-	NODE* z=allocNode(t-1);//allocating node with t-1 keys (y has n=2t-1 keys)
+	NODE* z=allocNode(t-1, T);//allocating node with t-1 keys (y has data=2t-1 keys)
     z->leaf=y->leaf;
     for(int j=0; j<t; j++) {
         z->key[j] = y->key[j + t]; //moving the right half of keys from y to z #|until j+t-1th|
@@ -77,53 +78,64 @@ void BTreeSplitChild(NODE* x, int i, NODE* y){//y is full node; x is its nonfull
     x->n=x->n+1; //we just added one another element to x on ith place, right?
 }
 
-void BTreeInsertNonfull(NODE* x, KEY k, int t){ //adding element [k] to the subtree with the non-full root [x]
-	int i=x->n-1;
-	if(x->leaf){//if x is the leaf, we just add key k to it
-		while(i>=1 && compareKeys(x->key[i],k)>0) {//сдвигаем элементы вправо
-			x->key[i] = x->key[i-1];//moving the keys right
-			i--;
-		}
-        if(compareKeys(x->key[i], k)==0){//key already exists in tree
-            x->key[i].n=x->key[i].n+1;
+void BTreeInsertNonfull(NODE* x, KEY* k, TREE* T){ //adding element [k] to the subtree with the non-full root [x]
+	int i=x->n;
+	if(x->leaf) {//if x is the leaf, we just add key k to it
+        if (x->n != 0) {
+            while (i >= 1 && compareKeys(x->key[i-1], k) > 0) {//сдвигаем элементы вправо
+                x->key[i] = x->key[i - 1];//moving the keys right
+                i--;
+            }
+            if (compareKeys(x->key[i-1], k) == 0 && i!=1) {//key already exists in tree
+                x->key[i-1]->data = x->key[i-1]->data + 1;
+            } else {
+                x->key[i] = k; //вставляем наш ключ справа
+                x->n = x->n + 1;
+            }
+        } else { //empty node
+            x->key[i] = k; //вставляем наш ключ справа
+            x->n = x->n + 1;
         }
-        else{
-            //i=0 or k>=x->key[i]
-            x->key[i]=k; //вставляем наш ключ справа
-            x->n=x->n+1;
-            //DiskWrite(x) - not shure
-        }
-	}
+    }
 	else{//or we are adding x to the subtree, whose root is the x's child, searching for the appropriate child
         while(i>=1 && compareKeys(x->key[i],k)>0)
             i--;
         i++;
-        if(x->c[i]->n==(2*t-1)){ //if the child is the full node, then we split it to the 2 half-fool nodes
-            BTreeSplitChild(x, i, x->c[i]);
+        if(x->c[i]->n==(2*T->t-1)){ //if the child is the full node, then we split it to the 2 half-fool nodes
+            BTreeSplitChild(x, i, x->c[i], T);
             if(compareKeys(k, x->key[i])<0)//determining in which of the new subtrees' we are adding x
                 i++;
         }
-        BTreeInsertNonfull(x->c[i], k, t);
-
+        BTreeInsertNonfull(x->c[i], k, T);
 	}
 }
 
-int compareKeys(KEY key1, KEY key2){
-    int i=0;
-	while(*(key1.key+i)==*(key2.key+i)){
-		i++;
-		if(*(key1.key+i)=='\0' || *(key2.key+i)=='\0') return 0;
-	}
-	if(*(key1.key+i)>*(key2.key+i)) return 1;
-	else return -1;
+NODE * BTreeInsert(TREE *T, KEY* k){
+    NODE* r=T->root;
+    NODE* s;
+    if(r->n==(2*T->t-1)){ //if the root has maximum number of children
+        //NODE* s=(NODE*) malloc(sizeof(NODE));//ALLOCATING FOR THE NEW ROOT | NO ALLOCATION FOR KEYS; data=0
+        //s->c=(NODE**) malloc(sizeof(NODE*)*(s->data+1));//ALLOCATING FOR THE CHILDREN LINKS
+        s=allocNode(1, T);
+        T->root=s;
+        s->leaf=0;
+        s->n=0;
+        s->c[0]=r;//growing up a new root, pointing the old one to it as a child
+        BTreeSplitChild(s, 0, r, T); //connecting to the main root to the first position; i IS THE INDEX NOT THE NUMBER OF POSITION
+        BTreeInsertNonfull(s, k, T->t);
+    }
+    else
+        BTreeInsertNonfull(r,k,T->t);
+    return s;
 }
 
-int deleteElement(){
-
-}
-
-NODE* getElement(){
-
+TREE* createBtree(int t){
+    TREE* T=malloc(sizeof(TREE));
+    T->t=t;
+    T->root=allocNode(0, T);
+    T->root->leaf=1;
+    T->root->n=0;
+    return T;
 }
 
 //TODO: iterator
