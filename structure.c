6e2706d7b7cbfe8891051833b7ea7491a8088d77
pekashy <klever.kl@yandex.ru>
2018-03-01 11:58:52 +0300

    headers init commit

--- structure.c
+++ structure.c
@@ -1,6 +1,36 @@
 #include <stdio.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <zconf.h>
+#include <stdlib.h>
+#include <ctype.h>
 #include "structure.h"
 
 int main(){
-	printf("hello world %d\n", funcThatReturns5());
+    char filename[]="source";
+    int file=open(filename, O_RDONLY);
+    printf("hello %d\n", file);
+    struct stat st;
+    stat(filename, &st);
+    printf("hello %d\n", st.st_size);
+    char* word=malloc(((int) st.st_size)*sizeof(char));
+    char* it=word;
+    char c;
+    //printf("hello %d\n", sizeof(TREE));
+    //NODE* T=listInit(3);
+    while(read(file, &c, 1)!=0){
+        if((c==' ' || c=='\n' || c=='-') && strlen(word)!=0){
+            addNode(T, word);
+            printf("%s\n", word);
+            memset(word, '\0', ((int) st.st_size)*sizeof(char));
+            it=word;
+        }
+        if(c>=65 && c<=90) c=(char) tolower(c);
+        //printf("%c %d %s\n", c, c, word);
+        if(c>=97 && c<=122){
+            *it=c;
+            it++;
+        }
+    }
+
 }
--- structure.h
+++ structure.h
@@ -1,5 +1,40 @@
 #include <stdio.h>
 
-int funcThatReturns5 (){
-	return 7;
+typedef struct Node NODE;
+
+typedef struct Key{
+    char* key;
+    int n;
+} KEY;
+
+struct Node{
+    KEY* key;
+    NODE* next;
+    NODE* prev;
+};
+
+typedef struct Iterator{
+    NODE* (*next)();
+    NODE* current;
+    int isEnd;
+} ITERATOR;
+
+NODE* listInit(char* key){
+
+}
+
+NODE* listAdd(char* key){
+
+}
+
+NODE* listInsert(NODE* head, NODE* x){
+
+}
+
+NODE* listDelete(NODE* x){
+
+}
+
+NODE* nextElement(ITERATOR* i){
+
 }
