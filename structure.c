897bc7728b5373cb1c87b76aa5312d98702953f9
pekashy <klever.kl@yandex.ru>
2018-03-11 15:13:01 +0300

    init commit. Element insertion and basic test data generation added

--- structure.c
+++ structure.c
@@ -1,6 +1,27 @@
 #include <stdio.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <zconf.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+#include <time.h>
 #include "structure.h"
 
 int main(){
-	printf("hello world %d\n", funcThatReturns5());
+    char filename[]="source";
+    int file=open(filename, O_RDONLY);
+    printf("hello %d\n", file);
+    struct stat st;
+    stat(filename, &st);
+    printf("hello %d\n", st.st_size);
+    char* word=malloc(((int) st.st_size)*sizeof(char));
+    char* it=word;
+    char c;
+    //printf("hello %d\n", sizeof(TREE));
+    TREE* tree=createTree();
+    srand(time(NULL));//temporary
+    for(int i=0; i<10; i++){
+        addNode(tree, rand() % (100 + 1 - 1) + 1);
+    }
 }
--- structure.h
+++ structure.h
@@ -1,5 +1,91 @@
 #include <stdio.h>
 
-int funcThatReturns5 (){
-	return 7;
+typedef struct Node NODE;
+
+typedef struct Key{
+    int key;
+    int n;
+} KEY;
+
+struct Node{
+    KEY* key;
+    NODE* left;
+    NODE* right;
+    int inList; //must be 1 or isNil
+};
+
+typedef struct Tree{
+    NODE* root;
+} TREE;
+
+void freeNode(NODE* n);
+NODE* treeInsert(TREE* t, NODE* z);
+KEY* createKey(int k);
+NODE* createNode();
+
+TREE* createTree(){
+    TREE *t=malloc(sizeof(TREE));
+    t->root=createNode();
+    t->root->inList=1;
+    return t;
+}
+
+NODE* createNode(){
+    NODE* n=malloc(sizeof(NODE));
+    n->inList=0;
+    return n;
+}
+
+
+NODE* addNode(TREE* t, int k){
+    NODE* n=createNode();
+    KEY* key =createKey(k);
+    n->key=key;
+    return treeInsert(t, n);
+}
+
+NODE* treeInsert(TREE* t, NODE* z){
+    NODE* y=createNode();
+    NODE* x=t->root;
+    while(x->inList){
+        y=x;
+        if(z->key->key<x->key->key)
+            x=x->left;
+        if(z->key->key==x->key->key){
+            x->key->n=x->key->n+1;
+            freeNode(z);
+            return x;
+        }
+        if(z->key->key>x->key->key)
+            x=x->right;
+    }
+    //p[z]=y ???
+    if(!y->inList){
+        t->root=z;
+        freeNode(y);
+    }
+    else{
+        if(z->key->key<y->key->key)
+            y->left=z;
+        if(z->key->key==x->key->key){
+            x->key->n=x->key->n+1;
+            freeNode(z);
+            return x;
+        }
+        if(z->key->key>x->key->key)
+            y->right=z;
+    }
+    return z;
+}
+
+void freeNode(NODE* n){
+    free(n->key);
+    free(n);
+}
+
+KEY* createKey(int k){
+    KEY* key=malloc(sizeof(KEY));
+    key->n=1;
+    key->key=k;
+    return key;
 }
